e076f02324bbe25463c3ebeb62f3720d938b6a52
Sebastian Bolanos <sebasheston@gmail.com>
2018-08-28 15:57:18 -0700

    Add tests for the getters

--- test/RenewalFeeEscrow.test.js
+++ test/RenewalFeeEscrow.test.js
@@ -1,30 +1,78 @@
 const RenewalFeeEscrow = artifacts.require('./RenewalFeeEscrow.sol')
 
-require('chai').should()
+require('chai').use(require('chai-bignumber')(web3.bigNumber)).should()
 
 const expectEvent = require('./helpers/expectEvent.js')
 const { assertRevert } = require('./helpers/assertRevert.js')
 
 contract('RenewalFeeEscrow', (accounts) => {
 
-  let subnetDAO = accounts[9]
-  let subnetDAOTwo = accounts[8]
+  let subnetDAO = accounts[accounts.length-1]
+  let subnetDAOTwo = accounts[accounts.length-2]
   let contract
 
-  describe('New bill', function() {
+  describe.only('getCountOfSubscribers and getCountOfCollectors', async () => {
 
-    beforeEach(async function() {
+    let min = Math.ceil(7)
+    let max = Math.floor(2)
+    let subnetDAOUsers = Math.floor(Math.random() * (max - min)) + min
+    let subnetDAOTwoUsers = Math.floor(Math.random() * (max - min)) + min
+
+    beforeEach(async () => {
+
+      contract = await RenewalFeeEscrow.new(subnetDAO)
+
+      for (let i = 0; i < subnetDAOUsers; i++) {
+        await contract.addBill(subnetDAO, 1*(10**10), {from: accounts[i], value: 1*(10**18)})
+      }
+      for (let i = 0; i < subnetDAOTwoUsers; i++) {
+        await contract.addBill(subnetDAOTwo, 1*(10**10), {from: accounts[i], value: 1*(10**18)})
+      }
+    })
+
+    it('Should have the right length', async () => {
+      let subscribers = await contract.getCountOfSubscribers(subnetDAO)
+      subscribers.toNumber().should.eql(subnetDAOUsers)
+
+      let subscribersTwo = await contract.getCountOfSubscribers(subnetDAOTwo)
+      subscribersTwo.toNumber().should.eql(subnetDAOTwoUsers)
+    })
+
+    it('Each user should have the right amount of collectors', async () => {
+
+      let smallerDAO
+      let biggerDAO
+      if (subnetDAOUsers < subnetDAOTwoUsers) {
+        smallerDAO = subnetDAOUsers
+        biggerDAO = subnetDAOTwoUsers
+      } else {
+        smallerDAO = subnetDAOTwoUsers
+        biggerDAO = subnetDAOUsers
+      }
+
+      for (var i in biggerDAO) {
+        let totalBills = await contract.getCountOfCollectors(accounts[i])
+        if (i < smallerDAO) {
+          totalBills.toNumber().should.eql(2)
+        } else {
+          totalBills.toNumber().should.eql(1)
+        }
+      }
+
+    })
+  })
+
+  describe('newBill', async () => {
+
+    beforeEach(async () => {
       contract = await RenewalFeeEscrow.new(subnetDAO)
     })
 
-    it('Adds a new bill to mapping', async function() {
+    it('Adds a new bill to mapping', async () => {
 
-      let txn = {
-        from: accounts[0],
-        value: 1*(10**18) // 1 ether
-      }
-
-      const receipt = await contract.addBill(subnetDAO, 1*(10**10), txn)
+      const receipt = await contract.addBill(
+        subnetDAO, 1*(10**10), {from: accounts[0], value: 1*(10**18)}
+      )
       const event = await expectEvent.inLogs(receipt.logs, 'NewBill', { 
         payer: accounts[0],
         collector: accounts[9]
@@ -33,11 +81,22 @@ contract('RenewalFeeEscrow', (accounts) => {
       event.args.collector.should.eql(subnetDAO)
     })
 
-    it('Will not replace an exsting bill', async function() {
+    it('Will not replace an exsting bill', async () => {
       await contract.addBill(subnetDAO, 1*(10**10), {from: accounts[0], value: 1*(10**18)})
       assertRevert(contract.
         addBill(subnetDAO, 2*(10**10), {from: accounts[0], value: 2*(10**18)})
       )
+    })
+
+    it('subscribersOfPayee should have an equal length to txns', async () => {
+      await contract.addBill(subnetDAO, 1*(10**10), {from: accounts[0], value: 1*(10**18)})
+      await contract.addBill(subnetDAO, 1*(10**10), {from: accounts[1], value: 1*(10**18)})
+      await contract.addBill(subnetDAO, 1*(10**10), {from: accounts[2], value: 1*(10**18)})
+      await contract.addBill(subnetDAO, 1*(10**10), {from: accounts[3], value: 1*(10**18)})
+      await contract.addBill(subnetDAO, 1*(10**10), {from: accounts[4], value: 1*(10**18)})
+      const subscribersOfPayee = await contract.subscribersOfPayee(subnetDAO, 2);
+      console.log('HIII', subscribersOfPayee)
+
     })
 
   })
@@ -60,6 +119,9 @@ contract('RenewalFeeEscrow', (accounts) => {
     })
 
     it(`Revert when Subnet doesn't have subscribers`, async () => {
+			assertRevert(
+				contract.collectSubnetFees({from: accounts[3]})
+			)
     })
 
     it('Subnet should have an expected ballance of all of its bills', async () => {
