477e2c6af52f0d84473d07c93bc65a4a1e15d93f
Sebastian Bolanos <sebasheston@gmail.com>
2018-08-27 19:32:40 -0700

    Remove extra tests

--- test/RenewalFeeEscrow.test.js
+++ test/RenewalFeeEscrow.test.js
@@ -1,26 +1,26 @@
 const RenewalFeeEscrow = artifacts.require('./RenewalFeeEscrow.sol')
 
+const { expectRevert } = requires('./helpers/expectEvent.js');
 
 contract('RenewalFeeEscrow', (accounts) => {
 
-  before(function() {
-  })
-
   let contract
-  beforeEach(async function() {
-    contract = await RenewalFeeEscrow.new()
+  before(async function() {
+    contract = await RenewalFeeEscrow.deployed()
   })
 
-  it('adds a new bill to mapping', async function() {
+
+  it('Adds a new bill to mapping', async function() {
 
     let txn = {
       from: accounts[0],
-      value: 1*(10**18)
+      value: 1*(10**18) // 1 ether
     }
     await contract.addBill(accounts[1], 1*(10**10), txn)
-    let bill = await contract.billMapping(accounts[0], accounts[1])
-    console.log("Bill", bill)
-
-    assert()
+    let receipt = await contract.billMapping(accounts[0], accounts[1])
+    const event = expectEvent.inLogs(receipt.logs, 'NewBill', { 
+      payer: accounts[0],
+      collector: accounts[1]
+    })
   })
 })
