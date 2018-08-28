e79ae87e43401a331b2c2a29fbd76e05ee8111c0
Sebastian Bolanos <sebasheston@gmail.com>
2018-08-28 00:05:16 -0700

    Add placeholder tests

--- test/RenewalFeeEscrow.test.js
+++ test/RenewalFeeEscrow.test.js
@@ -42,14 +42,63 @@ contract('RenewalFeeEscrow', (accounts) => {
 
   })
 
-  describe('Collect SubnetFees', async () => {
-    before('Generate some bills', async () => {
-      let txn = {
-        from: accounts[0],
-        value: 1*(10**18) // 1 ether
-      }
-      await contract.addBill(subnetDAO, 1*(10**10), txn)
+  describe('collectSubnetFees', async () => {
 
+    let txnCount
+    beforeEach(async() => {
+      contract = await RenewalFeeEscrow.new(subnetDAO)
+      await contract.addBill(subnetDAO, 1*(10**10), {from: accounts[0], value: 1*(10**18)})
+      await contract.addBill(subnetDAOTwo, 1*(10**9), {from: accounts[0], value: 1*(10**15)})
+
+      let min = Math.ceil(16)
+  		let max = Math.floor(4)
+  		txnCount = Math.floor(Math.random() * (max - min)) + min
+			//Generate some fake blocks
+      for (var i; i <= txnCount; i++) {
+        await RenewalFeeEscrow.new(subnetDAO)
+      }
     })
+
+    it(`Revert when Subnet doesn't have subscribers`, async () => {
+    })
+
+    it('Subnet should have an expected ballance of all of its bills', async () => {
+    })
+
+    it('Account of bill should be zero when it cant afford', async () => {
+    })
+
+    it('Set the account of bill to zero when it cant afford', async () => {
+    })
+
+    it('Bill should have lastUpdated with same blockNumber as latest block', async () => {
+    })
+
+
+  })
+
+  describe('payMyBills', async () => {
+
+    let txnCount
+    beforeEach(async() => {
+      contract = await RenewalFeeEscrow.new(subnetDAO)
+      await contract.addBill(subnetDAO, 1*(10**10), {from: accounts[0], value: 1*(10**18)})
+      await contract.addBill(subnetDAOTwo, 1*(10**9), {from: accounts[0], value: 1*(10**15)})
+
+      let min = Math.ceil(16)
+  		let max = Math.floor(4)
+  		txnCount = Math.floor(Math.random() * (max - min)) + min
+			//Generate some fake blocks
+      for (var i; i <= txnCount; i++) {
+        await RenewalFeeEscrow.new(subnetDAO)
+      }
+    })
+
+    it('Subscriber should have each of the bills with an expect amount', async () => {
+    })
+
+    it('Bill should have lastUpdated with same blockNumber', async () => {
+    })
+
   })
 })
